61e0137573e705cf82ae901ea3ab197c7f3e4108
unknown <praveenkumarsundarraj@gmail.com>
2026-03-24 00:53:20 +0800

    all possible cucumber features

--- features/support/hooks.ts
+++ features/support/hooks.ts
@@ -5,25 +5,25 @@ import { POManager } from "../../pages/POManager";
 import { expect } from "playwright/test";
 //here i have declared two different before execution corresponding to the tagged scenarios to be executed.
 //Note: Same tag name should be added to the test cases
-Before(async function(this: CustomManager)
+Before({tags:"@validation"}, async function(this: CustomManager)
 {
     // here i initialize which i created in custom Manager
-    const browser =await chromium.launch({headless: false, timeout: 5000});
+    const browser =await chromium.launch({headless: true, timeout: 5000});
     this.context =await browser.newContext();
     this.page = await this.context.newPage();
     this.expect = expect;
     this.poManager = new POManager(this.page, this.expect);
 });
 
-// Before({tags:"@regression"}, async function(this: CustomManager)
-// {
-//     // here i initialize which i created in custom Manager
-//     const browser =await chromium.launch({headless: true, timeout: 5000});
-//     this.context =await browser.newContext();
-//     this.page = await this.context.newPage();
-//     this.expect = expect;
-//     this.poManager = new POManager(this.page, this.expect);
-// });
+Before({tags:"@regression"}, async function(this: CustomManager)
+{
+    // here i initialize which i created in custom Manager
+    const browser =await chromium.launch({headless: true, timeout: 5000});
+    this.context =await browser.newContext();
+    this.page = await this.context.newPage();
+    this.expect = expect;
+    this.poManager = new POManager(this.page, this.expect);
+});
 
 After( async function(this: CustomManager)
 {
